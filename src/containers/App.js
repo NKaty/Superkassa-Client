1779a5ec04a959f37520d0b45394a127467d70f7
NKaty <nd.katy@gmail.com>
2021-01-24 15:20:38 +0300

    add button logic

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,45 @@
-function App() {
-  return <div className="App" />
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import io from 'socket.io-client'
+import Button from '../components/Button'
+import Layout from '../components/Layout'
+import {
+  buttonStateSelector,
+  buttonErrorSelector,
+  subscribeOnButtonState,
+} from '../store/slices/button'
+const socket = io('/')
+
+const App = ({ isActive, error, subscribeOnButtonState }) => {
+  console.log('app')
+  useEffect(() => {
+    console.log('subscribe')
+    subscribeOnButtonState(socket)
+  }, [subscribeOnButtonState])
+
+  const toggleHandler = event => {
+    event.preventDefault()
+    socket.emit('toggle-state')
+  }
+
+  return (
+    <Layout>
+      <Button
+        btnActive={isActive}
+        onClickHandler={toggleHandler}
+        disabled={error}
+      />
+    </Layout>
+  )
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    isActive: buttonStateSelector(state),
+    error: buttonErrorSelector(state),
+  }
+}
+
+export default connect(mapStateToProps, {
+  subscribeOnButtonState,
+})(App)
---
+++ src/store/slices/button.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const buttonSlice = createSlice({
+  name: 'button',
+  initialState: {
+    isActive: false,
+    error: false,
+  },
+  reducers: {
+    getButtonStateSuccess: (state, { payload }) => {
+      state.isActive = payload
+      state.error = false
+    },
+    getButtonStateFailure: state => {
+      state.error = true
+    },
+  },
+})
+
+export const {
+  getButtonStateSuccess,
+  getButtonStateFailure,
+} = buttonSlice.actions
+
+export default buttonSlice.reducer
+
+export const subscribeOnButtonState = socket => dispatch => {
+  if (socket) {
+    socket.on('set-state', data => dispatch(getButtonStateSuccess(data)))
+    socket.on('connect_error', () => dispatch(getButtonStateFailure()))
+  } else {
+    dispatch(getButtonStateFailure())
+  }
+}
+
+export const buttonStateSelector = state => {
+  return state.button.isActive
+}
+
+export const buttonErrorSelector = state => {
+  return state.button.error
+}
