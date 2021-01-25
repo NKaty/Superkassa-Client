5f88509210b244acdffaf3bdcad1d859214bdc0d
NKaty <nd.katy@gmail.com>
2021-01-25 14:46:34 +0300

    refactor variable names

--- src/store/slices/button.js
+++ src/store/slices/button.js
@@ -4,33 +4,30 @@ const buttonSlice = createSlice({
   name: 'button',
   initialState: {
     isActive: false,
-    error: false,
+    socketError: false,
   },
   reducers: {
-    getButtonStateSuccess: (state, { payload }) => {
+    setButtonState: (state, { payload }) => {
       state.isActive = payload
-      state.error = false
+      state.socketError = false
     },
-    getButtonStateFailure: state => {
-      state.error = true
+    setSocketError: state => {
+      state.socketError = true
     },
   },
 })
 
-export const {
-  getButtonStateSuccess,
-  getButtonStateFailure,
-} = buttonSlice.actions
+export const { setButtonState, setSocketError } = buttonSlice.actions
 
 export default buttonSlice.reducer
 
 export const subscribeOnButtonState = socket => dispatch => {
   if (socket) {
-    socket.on('set-state', data => dispatch(getButtonStateSuccess(data)))
-    socket.on('connect_error', () => dispatch(getButtonStateFailure()))
+    socket.on('set-state', data => dispatch(setButtonState(data)))
+    socket.on('connect_error', () => dispatch(setSocketError()))
     socket.emit('new-connection')
   } else {
-    dispatch(getButtonStateFailure())
+    dispatch(setSocketError())
   }
 }
 
@@ -39,5 +36,5 @@ export const buttonStateSelector = state => {
 }
 
 export const buttonErrorSelector = state => {
-  return state.button.error
+  return state.button.socketError
 }
